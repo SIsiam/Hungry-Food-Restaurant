9c7eb564e765e4dafe052160e906b8e63eb1741e
Akhtaruzzaman <akhtar01947@gmail.com>
2021-05-30 15:58:19 +0600

    model update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,21 @@ import React from 'react';
 import Events from '../components/Events/Events';
 import Foods from '../components/Food/Foods';
 import Header from '../components/Hero/Header';
+import Modal from '../components/Model/Model';
 import Reviews from '../components/Reviews/Reviews';
 import Service from '../components/services/Service';
 
 const Home = () => {
 	return (
-		<header>
-			<Header />
-			<Foods />
-			<Events />
-			<Reviews />
-			<Service />
-		</header>
-	);
+    <header>
+      <Header />
+      <Foods />
+      <Events />
+      <Service />
+      <Modal />
+      <Reviews />
+    </header>
+  );
 };
 
 export default Home;
